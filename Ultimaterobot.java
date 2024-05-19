204734e8c192b35542515ddc42b4f9084fdbef6a
japan <tanakornmankhetwit@gmail.com>
2024-05-19 20:47:25 +0700

    honey, new robot just dropped

--- Ultimaterobot.java
+++ Ultimaterobot.java
@@ -1,64 +1,78 @@
 package aigroupwork;
+
 import robocode.*;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-import java.awt.*;
 
-// Insert yourname here
-// Burin Intachuen
-// Possathorn Sujipisut
+public class Ultimaterobot extends AdvancedRobot  {
 
-//import java.awt.Color;
+	//Corner, 0=topLeft, 90=topRight, 180=bottomRight, 270(-90)=bottomLeft, static to keep in between rounds
+	static int corner = 0;
+	//Gun Direction for turning around or looking at enemy
+	int gunDirection = 1;
+	//Number of other robot in the field for calculations on death
+	int others;
+	//stop the robot from shooting at other robot
+	boolean stopWhenSeeRobot = false;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-
-/**
- * Ultimaterobot - a robot by (your name here)
- */
-public class Ultimaterobot extends Robot
-{
-	/**
-	 * run: Ultimaterobot's default behavior
-	 */
 	public void run() {
-		// Initialization of the robot should be put here
-		setAdjustGunForRobotTurn(true);
-		setAdjustRadarForGunTurn(true);
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		//go to corner first before fighting
+		goCorner();
+		//get number of other robot
+		others = getOthers();
 
-		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
+		// Turns the gun infinitely, looking for enemies
+		while (true) {
 			turnGunRight(360);
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(10);
+		if (stopWhenSeeRobot) {
+			// Turn the robot towards the enemy
+			setTurnRight(e.getBearing());
+			// Shoots always that it's aiming at the enemy
+			setFire(3);
+			// And move forward
+			setAhead(100);
+			// Inverts the gun direction on each turn
+			gunDirection = -gunDirection;
+			// Turn 360 degrees (clockwise or anti-clockwise)
+			setTurnGunRight(360 * gunDirection);
+			// Execute all the pending actions
+			execute();
+		}
 	}
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+	public void goCorner() {
+		stopWhenSeeRobot = false;
+		// turn to face the wall to the "right" of our desired corner.
+		turnRight(normalRelativeAngleDegrees(corner - getHeading()));
+		// Move to that wall
+		ahead(5000);
+		// Turn to face the corner
+		turnLeft(90);
+		// Move to the corner
+		ahead(5000);
+		// Turn gun to starting point
+		turnGunLeft(90);
+		// Can shoot at other robots now
+		stopWhenSeeRobot = true;
 	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+
+	public void onDeath(DeathEvent e) {
+		// Well, others should never be 0, but better safe than sorry.
+		if (others == 0) {
+			return;
+		}
+
+		// If 75% of the robots are still alive when we die, we'll switch corners.
+		if (getOthers() / (double) others >= .75) {
+			corner += 90;
+			if (corner == 270) {
+				corner = -90;
+			}
+		}
+	}
 }
